d3e0e6c464583b203453b5b5fefba91a2b9818e9
Luis Rodriguez <luis-1995@live.com.ar>
2021-08-22 18:04:06 -0500

    Documentacion hasta 22-ago-2021

---
+++ SEMANA2/OPERACIONES_JS/calculadora1.js
@@ -0,0 +1,19 @@
+function sumar(num_1, num_2){
+    return num_1 + num_2;
+}
+
+function restar(num_1, num_2){
+    return num_1 - num_2;
+}
+
+function multiplicar(num_1, num_2){
+    return num_1 * num_2;
+}
+
+function division(num_1, num_2){
+    return num_1 / num_2;
+}
+
+console.log("-------------- Testeo de Operaciones / Calculadora --------------");
+
+console.log(3**2);
---
+++ SEMANA2/OPERACIONES_JS/operaciones.js
@@ -0,0 +1,11 @@
+function test(x, y){
+    return x*2;
+    console.log(x);
+    return x / 2;
+}
+
+console.log(test(3))
+
+function centimeterToInches(x){
+    return 
+}
