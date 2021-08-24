4ac2979fa333f1589c472613d7b91461f1b71e7c
Luis Fernando Rodriguez Ortiz <luis-1995@live.com.ar>
2021-08-24 16:22:00 -0500

    segundos cambios

--- MODULO2/ACTIVIDAD_24-AGO-2021_INTRO_JS/operaciones.js
+++ MODULO2/ACTIVIDAD_24-AGO-2021_INTRO_JS/operaciones.js
@@ -1,5 +1,6 @@
 //Punto 1
-/*console.log(!1); // Devuelve falso
+console.log("\n"+"Punto1"+"\n");
+console.log(!1); // Devuelve falso
 console.log(!!1); // devuelve true
 console.log(!0); // devuelve true
 console.log(!!""); // Devuelve falso
@@ -9,10 +10,10 @@ let y = 9;
 console.log("Punto a.", x < 10 && x!==5);
 console.log("Punto b.",x>9 || x===5);
 console.log("Punto c.",!(x===y));
-*/
+
 
 //Punto 2
-/*
+
 console.log("\n"+"Punto2"+"\n");
 let x=10
 let y ="a"
@@ -22,7 +23,7 @@ console.log(y==="b" || x >= 10); //devuelve true
 let x=3
 let y=8
 console.log(!(x == "3" || x === y) && !(y !== 8 && x <= y)); //devuelve false
-*/
+
 
 let str = ""
 let msj = "jaja!"
