ec11ca71a34407aba7bc000fb7f262707a4201f8
Luis Fernando Rodriguez Ortiz <luis-1995@live.com.ar>
2021-08-24 15:41:16 -0500

    cargando actividades desde PC oficina

---
+++ SEMANA2/Clase4/ejercicio.js
@@ -0,0 +1,32 @@
+//Actividad 1
+
+const horarioDeDespertar = 7.30;
+let tomoCafe = true;
+let lavoLosDientes = true;
+let horarioDeSalida = 8.30;
+let busACoger = "Ruta 4";
+console.log(tomoCafe, "\n"+ lavoLosDientes, "\n"+ horarioDeSalida, "\n"+ busACoger, "\n");
+
+//Actividad 2 "Sacar a pasear al perro"
+let checkBolsas = true;
+let nombrePerro = "Zeus";
+let llamoPerro = true;
+let colocoCorrea = true;
+let tiempoPaseo = 30; //minutos
+let juguetes = "huesito";
+let vuelvoACasa = true;
+console.log(checkBolsas, "\n"+ nombrePerro,  llamoPerro, "\n"+ colocoCorrea, "\n"+ tiempoPaseo, "\n"+ juguetes, "\n"+ vuelvoACasa );
+
+//Desafío extra
+
+let cajonPantalones = "Remeras";
+let cajonRemeras = "Pantalones";
+let cajonVacio;
+
+cajonVacio = cajonPantalones; //llevo remeras al cajón vacío
+cajonPantalones = cajonRemeras; // cambio pantalones al cajón de los pantalones
+cajonRemeras = cajonVacio //Paso remeras a cajón remeras
+
+console.log("cajonPantalones:",  cajonPantalones, "\n"+ "cajonRemeras:", cajonRemeras);
+
+
---
+++ SEMANA2/Node/prueba.js
@@ -0,0 +1,56 @@
+//tipo de variables
+
+/*var nombre = 'Julieta';
+let apellido = 'Vittori';
+const NACIONALIDAD = "Argentina";*/
+
+
+
+
+
+// Tipos de datos
+let nombre = 'Julieta'; //string comilla simple
+console.log(nombre);
+
+let apellido = "Vittori"; //string comilla doble
+console.log(apellido);
+
+let edad = "34"; //numero entero
+console.log(edad);
+
+let altura = '1.76'; //numero decimal
+console.log(altura);
+
+let nacionalidadArgentina = true; //booleano true
+console.log(nacionalidadArgentina);
+
+let hinchaDeBoca = false; //booleano false
+console.log(hinchaDeBoca);
+
+let notANumber = NaN; //NaN not a number
+console.log(notANumber);
+
+let nulo = null; // null
+console.log(nulo);
+
+let indefinido;
+console.log(indefinido); //undefined
+
+//Objeto Literal
+
+let miComputadora = {
+    marca: 'Asus VivoBook', //atributo
+    modelo: 2020, //atributo
+    estaEncendida: true,
+
+};
+
+console.log(miComputadora);
+
+//Dato array
+
+let paises = [
+    "Argentina", "Venezuela",  "Colombia"
+];
+
+console.log(paises);
