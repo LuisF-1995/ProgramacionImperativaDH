fc2cf970a77998091a5163fc91b762b74bcd8dbb
Luis Rodriguez <luis-1995@live.com.ar>
2021-09-03 18:24:05 -0500

    Actualizando a 3 septiembre 2021

---
+++ MODULO2/CLASE8_26-AGO-2021/ACTIVIDAD1_CLASE8.js
@@ -0,0 +1,16 @@
+/* Crear una función puedeSubir() que reciba dos parámetros: 
+//altura de la persona y si viene acompañada. Debe retornar 
+//un valor booleano (TRUE, FALSE) que indique si la persona puede subirse o no,
+// basado en las siguientes condiciones:
+//Debe medir más o igual de 1,40m y menos de 2 metros.
+//Si mide menos de 1,40m hasta 1.20m, deberá venir acompañado.
+//Si mide menos de 1,20m, no podrá subir ni acompañado. 
+
+function puedeSubir(altura, acompaniado) {
+    if (1.40<=altura<2.00){ // siempre volver a llamar a la variable o parametro
+
+    } else if () {
+
+    } else {}
+}*/
+
